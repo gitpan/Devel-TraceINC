ec13963b9b05c0da75e03062b8374e57af74ac7e
Marcel Grünauer <marcel@cpan.org>
2010-03-26 22:23:36 +0000

    Import of MARCEL/Devel-TraceINC-1.100850 from CPAN.
    gitpan-cpan-distribution: Devel-TraceINC
    gitpan-cpan-version:      1.100850
    gitpan-cpan-path:         MARCEL/Devel-TraceINC-1.100850.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Devel/TraceINC.pm
+++ lib/Devel/TraceINC.pm
@@ -1,12 +1,10 @@
-package Devel::TraceINC;
-
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
 
-
-our $VERSION = '0.01';
-
+package Devel::TraceINC;
+our $VERSION = '1.100850';
+# ABSTRACT: Trace who is loading which perl modules
 
 BEGIN {
     unshift @INC, sub {
@@ -14,18 +12,24 @@ BEGIN {
         my ($package, $filename, $line) = caller;
         warn "$file loaded from package $package, file $filename, line $line\n";
     };
-    return;   # undef to indicate that require() should look further
+    return;    # undef to indicate that require() should look further
 }
-
-
 1;
 
 
 __END__
+=pod
+
+=for test_synopsis 1;
+__END__
 
 =head1 NAME
 
-Devel::TraceINC - trace who is loading which perl modules
+Devel::TraceINC - Trace who is loading which perl modules
+
+=head1 VERSION
+
+version 1.100850
 
 =head1 SYNOPSIS
 
@@ -46,39 +50,39 @@ able to track down who loads what, I wrote Devel::TraceINC.
 Just C<use()> the module and it will print a warning every time a module is
 searched for in C<@INC>, i.e., loaded.
 
-=head1 TAGS
+=head1 INSTALLATION
 
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<develtraceinc> tag.
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<bug-devel-traceinc@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Devel-TraceINC>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Devel-TraceINC/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Devel-TraceINC/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2002-2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
